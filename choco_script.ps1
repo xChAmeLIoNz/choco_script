ac89a512ce8f6e9a338bacb84c6b2b36b88a321f
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-06-22 17:48:36 +0200

    fixed issues

--- choco_script.ps1
+++ choco_script.ps1
@@ -1,21 +1,26 @@
-<# append '!' at the start before saving the script for final use #>
-$Global:policy = Get-ExecutionPolicy
+
+# If Chocolatey isn't installed, it will download and install it
 if(!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
-	
-    #$policy = Get-ExecutionPolicy
-    
 	if($policy -ne "Bypass") {
+        # Sets the Execution Policy to Bypass any prompt for the script
 		Set-ExecutionPolicy Bypass -Scope Process -Force
         Write-Output "Execution Policy set to 'Bypass'"
-        #Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 	}
+    # Downloads and installs the Chocolatey installation script
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
 }
 
-choco upgrade chocolatey
-##Get-Content ".\packages.txt" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/xChAmeLIoNz/choco_script/master/packages.txt | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{}
-Set-ExecutionPolicy $Global:policy -Scope Process -Force
-Write-Output "Execution Policy set to $Global:policy"
+# Upgrades Chocolatey
+choco upgrade chocolatey -y
+
+# This will look for a file called 'packages.txt' and iterate through it, installing each package
+# Get-Content ".\packages.txt" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
+
+# This will get the content of the file 'packages.txt' uploaded on GitHub
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/xChAmeLIoNz/choco_script/master/packages.txt | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
+
+# Sets the Execution Policy to RemoteSigned
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.2
+Set-ExecutionPolicy RemoteSigned -Scope Process -Force
+Write-Output "Execution Policy set to RemoteSigned"
