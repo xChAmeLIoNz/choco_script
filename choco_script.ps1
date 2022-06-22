adc175cda1b1ee44709569ca1496b1b65e0301dc
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-06-22 17:34:23 +0200

    fixed errors

--- choco_script.ps1
+++ choco_script.ps1
@@ -1,8 +1,8 @@
 <# append '!' at the start before saving the script for final use #>
-
+$Global:policy = Get-ExecutionPolicy
 if(!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
 	
-    $policy = Get-ExecutionPolicy
+    #$policy = Get-ExecutionPolicy
     
 	if($policy -ne "Bypass") {
 		Set-ExecutionPolicy Bypass -Scope Process -Force
@@ -11,5 +11,11 @@ if(!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
 	}
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 }
 
+choco upgrade chocolatey
+##Get-Content ".\packages.txt" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/xChAmeLIoNz/choco_script/master/packages.txt | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{}
+Set-ExecutionPolicy $Global:policy -Scope Process -Force
+Write-Output "Execution Policy set to $Global:policy"
