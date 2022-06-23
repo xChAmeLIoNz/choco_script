d2c286d54236a8e23f2b45c44e8507329c71a222
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-06-23 11:37:04 +0200

    updated packages.txt

--- choco_script.ps1
+++ choco_script.ps1
@@ -1,11 +1,10 @@
 
 # If Chocolatey isn't installed, it will download and install it
 if(!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
-	if($policy -ne "Bypass") {
-        # Sets the Execution Policy to Bypass any prompt for the script
-		Set-ExecutionPolicy Bypass -Scope Process -Force
-        Write-Output "Execution Policy set to 'Bypass'"
-	}
+
+	# Sets the Execution Policy to Bypass any prompt for the script
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Write-Output "Execution Policy set to 'Bypass'"
     # Downloads and installs the Chocolatey installation script
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
@@ -15,7 +14,7 @@ if(!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
 choco upgrade chocolatey -y
 
 # This will look for a file called 'packages.txt' and iterate through it, installing each package
-# Get-Content ".\packages.txt" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
+#Get-Content ".\packages.txt" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
 
 # This will get the content of the file 'packages.txt' uploaded on GitHub
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/xChAmeLIoNz/choco_script/master/packages.txt | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
