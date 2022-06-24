6b5d18e61095c15f149ba6205c5091036eb71a48
xchamelionz <afogliiseppe@gmail.com>
2022-06-24 17:53:54 +0200

    updated choco and config scripts

--- choco_script.ps1
+++ choco_script.ps1
@@ -14,10 +14,10 @@ if(!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
 choco upgrade chocolatey -y
 
 # This will look for a file called 'packages.txt' and iterate through it, installing each package
-#Get-Content ".\packages.txt" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
+Get-Content ".\packages.txt" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
 
 # This will get the content of the file 'packages.txt' uploaded on GitHub
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/xChAmeLIoNz/choco_script/master/packages.txt | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
+#Invoke-WebRequest -Uri https://raw.githubusercontent.com/xChAmeLIoNz/choco_script/master/packages.txt | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install $_ -y}
 
 # Sets the Execution Policy to RemoteSigned
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.2
